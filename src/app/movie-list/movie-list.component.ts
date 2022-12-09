36d8b288e3b081483d33f4766283e4c0f306dcac
Vish Patel <vpatel@and.digital>
2022-12-09 16:02:52 +0000

    added skeleton

---
+++ src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css'],
+})
+export class MovieListComponent {
+  title = 'Movie List';
+}
